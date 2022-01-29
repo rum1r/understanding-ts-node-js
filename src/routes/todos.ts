cfbaee816967fef1afeb7509211366532198ef57
Rumiko Ueno <momotarouko@gmail.com>
2022-01-29 11:50:43 +0900

    195. コントローラの作成 & リクエストボディの取得

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,9 +1,10 @@
-import exp from 'constants';
 import { Router } from 'express';
+
+import { createTodo } from '../controllers/todos';
 
 const router = Router();
 
-router.post('/');
+router.post('/', createTodo);
 router.get('/');
 router.patch('/:id');
 router.delete('/:id');
